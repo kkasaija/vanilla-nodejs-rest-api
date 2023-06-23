53f0accc5c64dcbed3d59641b6b3a5f03a3024a3
Kasaija Kenneth <kasaijak@gmail.com>
2023-06-24 01:06:07 +0300

    get single user

--- controllers/productController.js
+++ controllers/productController.js
@@ -12,7 +12,18 @@ const getProducts = async (req, res) => {
     console.error(error);
   }
 };
-const getProduct = async (req, res) => {};
+const getProduct = async (req, res) => {
+  try {
+    const query = `SELECT * FROM users WHERE id = ${req.params.id}`;
+    await pool.query(query, (err, data) => {
+      if (!err) {
+        res.send(...data.rows);
+      } else throw err;
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
 const createProduct = async (req, res) => {};
 const updateProduct = async (req, res) => {};
 const deleteProduct = async (req, res) => {};
